83b908f4deaedd1c6db19f6a5bbc5b47a0d0af59
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-07-23 23:55:55 +0100

    Add event argument

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,15 +14,20 @@ class Counter extends Component {
   //   console.log('Increment Clicked', this)
   // }
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 })
+  }
+
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
   }
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={ () => this.handleIncrement(product) } className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
