56ed3644852793792d84a689e24f9201263dede5
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-07-24 10:55:37 +0100

    Compose another component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -19,15 +19,11 @@ class Counter extends Component {
     this.setState({ count: this.state.count + 1 })
   }
 
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
-  }
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-        <button onClick={ () => this.handleIncrement(product) } className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={ () => this.handleIncrement({ id: 1 }) } className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
