a0e28a2d12c5d803ab941540a3d0df1a883a289d
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-07-28 21:36:57 +0100

    Add decrement button and functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -24,10 +24,15 @@ class Counter extends Component {
   render() {
     console.log('Counter - Rendered')
     return (
-      <div>
-        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-        <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
-        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn ml-2 btn-danger btn-sm">Delete</button>
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">+</button>
+          <button onClick={() => this.props.onDecrement(this.props.counter)} className="btn ml-2 btn-warning btn-sm" disabled={this.checkZero()}>-</button>
+          <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn ml-2 btn-danger btn-sm">x</button>
+        </div>
       </div>
     );
   }
@@ -41,6 +46,11 @@ class Counter extends Component {
   formatCount() {
     const { value } = this.props.counter;
     return value === 0 ? 'Zero' : value;
+  }
+
+  checkZero() {
+    const { value } = this.props.counter;
+    return value === 0 ? true : false;
   }
 
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,7 +5,7 @@ class Counters extends Component {
   render() {
     console.log('Counters - Rendered')
 
-    const { onReset, counters, onDelete, onIncrement } = this.props
+    const { onReset, counters, onDelete, onIncrement, onDecrement } = this.props
     return (
       <div>
         <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
@@ -13,6 +13,7 @@ class Counters extends Component {
           <Counter key={counter.id}
             onDelete={onDelete}
             onIncrement={onIncrement}
+            onDecrement={onDecrement}
             counter={counter} />)}
       </div>
     );
