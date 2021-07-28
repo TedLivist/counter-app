dcc5a42d74a46e8f0498725735471d728cf20f62
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-07-28 16:35:12 +0100

    Make NavBar into Stateless Functional Component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-  render() { 
-    return ( 
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-        </a>
-      </nav>
-    );
-  }
+// This is a Stateless Functional Component
+// This is often used instead of `class...extends...render()`
+// when the Component returns a rather simple functionality,
+// without state or methods
+const NavBar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+      </a>
+    </nav>
+  );
 }
  
 export default NavBar;
