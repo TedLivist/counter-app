300f82fdfee4424d39d0a989d6d67d909cb64b4e
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-07-28 13:36:11 +0100

    Add navbar

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -35,7 +35,7 @@ class Counters extends Component {
   render() { 
     return (
       <div>
-        <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Delete All</button>
+        <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
         { this.state.counters.map(counter =>
           <Counter key={counter.id}
             onDelete={this.handleDelete}
